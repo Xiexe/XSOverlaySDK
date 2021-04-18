5af353bf6314c538cfed29e36360c69d6edaf7e7
Xiexe <jcmaddox1@gmail.com>
2021-04-18 10:53:35 -0400

    Force linear color space

---
+++ XSOverlaySDK/Scripts/Editor/SDKImportInitializer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class SDKImportInitializer : Editor
+{
+    [InitializeOnLoadMethod]
+    private static void InitializeOnLoad()
+    {
+        if (PlayerSettings.colorSpace != ColorSpace.Linear)
+        {
+            PlayerSettings.colorSpace = ColorSpace.Linear;
+        }
+    }
+}
